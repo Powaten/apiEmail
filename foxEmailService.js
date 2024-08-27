ec5fadec3a3113ebd798055e39c349fb6130bb2f
Powaten <131688852+Powaten@users.noreply.github.com>
2024-08-27 09:56:39 +0100

    Update foxEmailService.js

--- foxEmailService.js
+++ foxEmailService.js
@@ -39,6 +39,8 @@ async function foxEmailService(req, res) {
 
   // console.log(replyTo, emailParams);
 
+  console.loh(process.env.API_KEY2)
+
   const response = await mailerSend.email.send(emailParams);
 
   // console.log( response);
