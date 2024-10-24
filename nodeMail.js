0b4a762538801a5863f27e55fb0a4269ec0a4d2f
Ebuka Gabriel <ebukagab@gmail.com>
2024-10-24 06:51:02 +0100

    Upgraded the server

--- nodeMail.js
+++ nodeMail.js
@@ -1,32 +1,34 @@
 const nodeMailer = require("nodemailer");
 
 const nodeMailerSendEmail = async (data) => {
-  let count = 0;
-  const { sender, message, replyToEmail, subject, receiversMail } = data;
+  try {
+    let count = 0;
+    const { sender, message, replyToEmail, subject, receiversMail } = data;
 
-  const transport = nodeMailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: sender.email,
-      pass: sender.password,
-    },
-  });
+    const transport = nodeMailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: sender.email,
+        pass: sender.password,
+      },
+    });
 
-  const mailOptions = {
-    from: `${sender.name} ${sender.email}`,
-    to: receiversMail,
-    subject: subject,
-    text: message,
-    replyTo: replyToEmail ? replyToEmail : "",
-  };
+    const mailOptions = {
+      from: `${sender.name} ${sender.email}`,
+      to: receiversMail,
+      subject: subject,
+      text: message,
+      replyTo: replyToEmail ? replyToEmail : "",
+    };
 
-  const res = await transport.sendMail(mailOptions);
+    const res = await transport.sendMail(mailOptions);
 
-  count = count + 1;
-
-  console.log(res, count);
-
-  return res;
+    console.log(res, "successfull");
+    return res;
+  } catch (err) {
+    console.log(err, err.message);
+    return { message: "Something went wrong, try again", errorCode: "300" };
+  }
 };
 
 module.exports = nodeMailerSendEmail;
